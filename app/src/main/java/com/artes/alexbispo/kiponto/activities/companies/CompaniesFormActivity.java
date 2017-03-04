470d15fd4ee1aa5111e9a97843151581aaae8929
alexbispo <alexbispo.artes@gmail.com>
2017-03-04 15:16:40 -0300

    Starting the app.

---
+++ app/src/main/java/com/artes/alexbispo/kiponto/activities/companies/CompaniesFormActivity.java
@@ -0,0 +1,15 @@
+package com.artes.alexbispo.kiponto.activities.companies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.artes.alexbispo.kiponto.R;
+
+public class CompaniesFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_companies_form);
+    }
+}
