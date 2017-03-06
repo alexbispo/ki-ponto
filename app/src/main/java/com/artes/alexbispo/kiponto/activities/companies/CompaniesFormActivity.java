6f03235341e9b4c1599458f71b245289131f92de
alexbispo <alexbispo.artes@gmail.com>
2017-03-05 21:28:09 -0300

    implements DatePickerDialogFragment

--- app/src/main/java/com/artes/alexbispo/kiponto/activities/companies/CompaniesFormActivity.java
+++ app/src/main/java/com/artes/alexbispo/kiponto/activities/companies/CompaniesFormActivity.java
@@ -1,15 +1,69 @@
 package com.artes.alexbispo.kiponto.activities.companies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.artes.alexbispo.kiponto.R;
+import com.artes.alexbispo.kiponto.fragments.DatePickerDialogFragment;
 
-public class CompaniesFormActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class CompaniesFormActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private int year, month, day;
+    private Button btnStartDate;
+    private Button btnLeavingDate;
+    private int btnDatePickerId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_companies_form);
+
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        btnStartDate = (Button) findViewById(R.id.company_btn_start_date);
+        btnLeavingDate = (Button) findViewById(R.id.company_btn_leaving_date);
+
+        btnStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnDatePickerId = view.getId();
+                DatePickerDialogFragment fragment = DatePickerDialogFragment.getInstance(CompaniesFormActivity.this, year, month, day);
+                FragmentManager manager = getSupportFragmentManager();
+                fragment.show(manager, "");
+            }
+        });
+
+        btnLeavingDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnDatePickerId = view.getId();
+                DatePickerDialogFragment fragment = DatePickerDialogFragment.getInstance(CompaniesFormActivity.this, year, month, day);
+                FragmentManager manager = getSupportFragmentManager();
+                fragment.show(manager, "");
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int y, int m, int d) {
+        year = y;
+        month = m;
+        day = d;
+
+        if (R.id.company_btn_start_date == btnDatePickerId) {
+            btnStartDate.setText(day + "/" + (month + 1) + "/" + year);
+        } else if (R.id.company_btn_leaving_date == btnDatePickerId) {
+            btnLeavingDate.setText(day + "/" + (month + 1) + "/" + year);
+        }
     }
 }
---
+++ app/src/main/java/com/artes/alexbispo/kiponto/fragments/DatePickerDialogFragment.java
@@ -0,0 +1,37 @@
+package com.artes.alexbispo.kiponto.fragments;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import com.artes.alexbispo.kiponto.activities.companies.CompaniesFormActivity;
+
+/**
+ * Created by alex on 05/03/17.
+ */
+
+public class DatePickerDialogFragment extends DialogFragment {
+
+    private int year, month, day;
+    private DatePickerDialog.OnDateSetListener listener;
+
+    public static DatePickerDialogFragment getInstance(DatePickerDialog.OnDateSetListener listener, int year, int month, int day){
+        DatePickerDialogFragment instance = new DatePickerDialogFragment();
+        instance.year = year;
+        instance.month = month;
+        instance.day = day;
+        instance.listener = listener;
+        return instance;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new android.app.DatePickerDialog(getActivity(), listener, year, month, day);
+    }
+
+}
